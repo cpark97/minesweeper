7a3e30670548eb90ddfc97817b6a23420fd823f1
Park Changbeom <eric9710@naver.com>
2022-02-11 17:34:41 +0000

    Implement #10 - stop watch

--- src/MineSweeper.jsx
+++ src/MineSweeper.jsx
@@ -1,7 +1,79 @@
-import { useRef } from 'react';
+import { useRef, useReducer } from 'react';
 
 import { useMineField } from './useMineField';
 import Board from './Board';
+
+function stopWatchReducer(state, action) {
+  switch(action) {
+    case 'RESET':
+      if (state.state === 'RESET') {
+        return state;
+      }
+      return {state: 'RESET', elapsed: 0, last: 0};
+    
+    case 'RESUME':
+      if (state.state === 'RESUMED') {
+        return state;
+      }
+      return {state: 'RESUMED', elapsed: state.elapsed, last: Date.now()};
+
+    case 'UPDATE':
+      if (state.state !== 'RESUMED') {
+        return state;
+      }
+      else {
+        const now = Date.now();
+        return {state: 'RESUMED', elapsed: state.elapsed + (now - state.last), last: now};
+      }
+
+    case 'PAUSE':
+      if (state.state !== 'RESUMED') {
+        return state;
+      }
+      else {
+        const now = Date.now();
+        return {state: 'PAUSED', elapsed: state.elapsed + (now - state.last), last: now};
+      }
+  }
+
+  throw new Error('stopWatchReducer: action error');
+}
+
+function useStopWatch() {
+  const [state, dispatch] = useReducer(stopWatchReducer, {state: 'RESET', elapsed: 0, last: 0});
+  const intervalID = useRef(0);
+
+  const reset = () => {
+    if (intervalID.current > 0) {
+      clearInterval(intervalID.current);
+      intervalID.current = 0;
+    }
+    dispatch('RESET');
+  };
+  const resume = () => {
+    if (intervalID.current > 0) {
+      return;
+    }
+    dispatch('RESUME');
+    intervalID.current = setInterval(
+      () => {
+        dispatch('UPDATE');
+      },
+      1000
+    );
+  };
+  const pause = () => {
+    if (intervalID === 0) {
+      return;
+    }
+    clearInterval(intervalID.current);
+    intervalID.current = 0;
+
+    dispatch('PAUSE');
+  };
+
+  return {state: state.state, elapsed: state.elapsed, reset, resume, pause};
+}
 
 export function MineSweeper() {
   const {
@@ -17,6 +89,8 @@ export function MineSweeper() {
     resetMineField,
     setBoardConfig,
   } = useMineField(10, 10, 10);
+
+  const sw = useStopWatch();
 
   const rowCountInput = useRef(null);
   const columnCountInput = useRef(null);
@@ -35,6 +109,13 @@ export function MineSweeper() {
       </div>
       <button onClick={resetMineField}>reset</button>
       <span>{gameState}</span>
+      <div>
+        <span>{sw.elapsed / 1000}</span>
+        <button onClick={() => sw.resume()}>start</button>
+        <button onClick={() => sw.pause()}>pause</button>
+        <button onClick={() => sw.reset()}>reset</button>
+        <span>{sw.state}</span>
+      </div>
       <Board 
         cells={cells}
         cellStates={cellStates}
