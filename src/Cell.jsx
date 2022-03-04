59faa606b13c1c397041d1afb761ed8c086e2742
Park Changbeom <eric9710@naver.com>
2022-03-04 12:37:30 +0000

    Remove unused codes

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -2,10 +2,6 @@ function preventDefault(e) {
   e.preventDefault();
 }
 
-function mouse(e) {
-  console.log(e.type, e.button, e.buttons);
-}
-
 function Flag() {
   return (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -21,23 +17,6 @@ function Flag() {
 }
 
 function Mine() {
-  return (
-    <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="64" cy="64" r="47" fill="black"/>
-      <rect x="58" width="12" height="18" fill="black"/>
-      <rect x="58" y="110" width="12" height="18" fill="black"/>
-      <rect y="55" width="18" height="12" fill="black"/>
-      <rect x="110" y="55" width="18" height="12" fill="black"/>
-      <rect x="17" y="17" width="12" height="12" fill="black"/>
-      <rect x="99" y="17" width="12" height="12" fill="black"/>
-      <rect x="99" y="99" width="12" height="12" fill="black"/>
-      <rect x="17" y="99" width="12" height="12" fill="black"/>
-      <circle cx="49" cy="49" r="12" fill="white"/>
-    </svg>
-  );
-}
-
-function Mine2() {
   return (
     <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g fill="none" stroke="black" strokeWidth="10">
@@ -70,7 +49,7 @@ export function Cell(props) {
     if (props.value === 0) {
       displayValue = null;
     } else if (props.value === -1) {
-      displayValue = <Mine2 />;
+      displayValue = <Mine />;
     } else {
       displayValue = props.value;
       className.push(`board__cell--${props.value}`);
